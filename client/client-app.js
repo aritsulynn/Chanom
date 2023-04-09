453b182dd6cb10c02df4255601c454f2d6b84b92
ThitiwutBoss03 <thitiwutmcpeakz@gmail.com>
2023-04-09 16:18:17 +0700

    Boss

---
+++ client/client-app.js
@@ -0,0 +1,93 @@
+
+const express = require('express');
+const path = require('path');
+const port = 3030;
+const app = express();
+const axios = require('axios').default;     // used for WS
+
+const router = express.Router();
+app.use(router);
+
+// set the static file directory
+app.use('/', express.static(path.join(__dirname,'/static')));
+
+// This is needed for POST method
+router.use(express.json());
+router.use(express.urlencoded({extended: true}));
+
+router.get('/', (req, res) => {
+    console.log("Request at /");
+    res.status(200).send("Hello World! in plain text");
+})
+
+router.get('/home', (req, res) => {
+    console.log("Request at /home");
+    res.status(200).sendFile(path.join(__dirname,"/html/home.html"))
+})
+
+router.get('/search', (req, res) => {
+    console.log("Request at /search");
+    res.status(200).sendFile(path.join(__dirname,"/html/search.html"))
+})
+
+router.get('/login', (req, res) => {
+    console.log("Request at /signin");
+    res.status(200).sendFile(path.join(__dirname,"/html/login.html"))
+})
+
+router.get('/aboutus', (req, res) => {
+    console.log("Request at /aboutus");
+    res.status(200).sendFile(path.join(__dirname,"/html/aboutus.html"))
+})
+
+router.get('/pManage', (req, res) => {
+    console.log("Request at /pManage");
+    res.status(200).sendFile(path.join(__dirname,"/html/pManage.html"))
+})
+
+router.get('/uManage', (req, res) => {
+    console.log("Request at /uManage");
+    res.status(200).sendFile(path.join(__dirname,"/html/uManage.html"))
+})
+
+router.get('/detail1', (req, res) => {
+    console.log("Request at /detail1");
+    res.status(200).sendFile(path.join(__dirname,"/html/detail1.html"))
+})
+
+router.get('/detail2', (req, res) => {
+    console.log("Request at /detail2");
+    res.status(200).sendFile(path.join(__dirname,"/html/detail2.html"))
+})
+
+router.get('/detail3', (req, res) => {
+    console.log("Request at /detail3");
+    res.status(200).sendFile(path.join(__dirname,"/html/detail3.html"))
+})
+
+router.get('/detail4', (req, res) => {
+    console.log("Request at /detail4");
+    res.status(200).sendFile(path.join(__dirname,"/html/detail4.html"))
+})
+
+router.get('/detail5', (req, res) => {
+    console.log("Request at /detail5");
+    res.status(200).sendFile(path.join(__dirname,"/html/detail5.html"))
+})
+
+router.get('/test', (req, res) => {
+    axios.get('http://localhost:3000/selectadmin', {responseType: 'json'})
+    .then((res) => {
+        console.log(res.data)
+    })
+})
+
+// unspecified path
+/*router.use((req, res, next) => {
+    console.log("404: Invalid accessed");
+    res.status(404).send("Error")
+})*/
+
+app.listen(port, () => {
+    console.log('Server listening on port: ' + port);
+})
---
+++ server/server.js
@@ -0,0 +1,110 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const dotenv = require('dotenv');
+
+/* Config dotenv and router */
+const router = express.Router();
+app.use(router);
+dotenv.config();
+
+// This is needed for POST method
+router.use(express.json());
+router.use(express.urlencoded({extended: true}));
+
+/* Connection to MySQL */
+const mysql = require('mysql2');
+var connection = mysql.createConnection({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USERNAME,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
+});
+
+connection.connect(function(err) {
+    if(err) throw err;
+    console.log("Connected DB: " + process.env.MYSQL_DATABASE);
+}); 
+
+router.get('/', (req, res) => {
+    res.send("Hello world");
+})
+
+// insert
+router.post('/insert', (req, res) => {
+    let student = req.body.student
+
+    if(!student) {
+        return res.status(400).send({error: true, message: 'Please provide student information'})
+    }
+
+    connection.query("INSERT INTO personal_info SET ? ", student, (error, results) => {
+        if(error) throw error;
+        return res.send({error: false, data: results.affectedRows, message: 'New student has been added successfully'})
+    })
+})
+
+// update
+router.put('/update', (req, res) => {
+    console.log(req.body.student);
+    let student_id = req.body.student.StudentID;
+    let student = req.body.student;
+
+    if(!student_id || !student) {
+        return res.status(400).send({error: true, message: 'Please provide student information'})
+    }
+
+    connection.query("UPDATE personal_info SET ? WHERE StudentID = ?", [student, student_id], (error, results) => {
+        if(error) throw error;
+        return res.send({error: false, data: results.affectedRows, message: 'Student has been updated successfully'})
+    })
+})
+
+// delete
+router.delete('/delete', (req, res) => {
+    let student_id = req.body.student.StudentID;
+    if(!student_id) {
+        return res.status(400).send({error: true, message: 'Please provide student information'})
+    }
+    connection.query("DELETE FROM personal_info WHERE StudentID = ?", [student_id], (error, results) => {
+        if(error) throw error;
+        return res.send({error: false, data: results.affectedRows, message: 'Student has been deleted successfully'})
+    })
+})
+
+// select
+router.get('/selectchanom/:id', (req, res) => {
+    let pID = req.params.id;
+
+    if(!pID) {
+        return res.status(400).send({error: true, message: 'Please provide product information'})
+    }
+    connection.query("SELECT * FROM product WHERE pID = ?", [pID], (error, results) => {
+        if(error) throw error;
+        return res.send({error: false, data: results[0], message: 'Product retrieved'})
+    })
+
+})
+
+// select all
+router.get('/selectchanom', (req, res) => {
+
+    connection.query("SELECT * FROM product", (error, results) => {
+        if(error) throw error;
+        return res.send({error: false, data: results, message: 'product list'})
+    })
+
+})
+
+router.get('/selectadmin', (req, res) => {
+
+    connection.query("SELECT * FROM administrator", (error, results) => {
+        if(error) throw error;
+        return res.send({error: false, data: results, message: 'admin list'})
+    })
+
+})
+
+app.listen(process.env.PORT, function() {
+    console.log("Server listening on port: " + process.env.PORT);
+});
