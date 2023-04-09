7e867152204ae4964f2d56e9b25c49d7813f2df0
Hyme1418 <him.wasabi@gmail.com>
2023-04-09 16:34:15 +0700

    test

--- Chanom.sql
+++ Chanom.sql
@@ -38,7 +38,8 @@ VALUES (1, "Classic Bubble Milk Tea", "Bubble Milk Tea", "Brown Sugar", 5, 60),
 INSERT INTO administrator
 VALUES ("Kritchanapat", "Kritchanapat", "Junju", "2003-05-26", "kritchanapat.jun@student.mahidol.edu"),
 ("Thitiwut", "Thitiwut", "Harnphatcharapanukorn", "2003-04-08", "thitiwutharn@gmail.com"),
-(""),
+<<<<<<< Updated upstream
+("Sirasit", "Sirasit", "Puangpathanachai", "2003-11-18", "sirasit.pun@student.mahidol.edu"),
 ("")
 ("");
 
